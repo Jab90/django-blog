b0143cf93f27dac8ae8425779fb01d74aaf97087
Jabbeer  Jeerooburkhan <jabbeer@googlemail.com>
2024-07-22 07:08:01 +0000

    added all links and requirements.txt

--- codestar/settings.py
+++ codestar/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['8000-jab90-djangoblog-rwxyusmuoi4.ws-eu115.gitpod.io','.herokuapp.com']
+ALLOWED_HOSTS = ['8000-jab90-djangoblog-myxlq3kqyn0.ws-eu115.gitpod.io','.herokuapp.com']
 
 
 # Application definition
